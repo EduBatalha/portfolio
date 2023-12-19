6b1ff582c0ba6469142f2e74e30269784d70c7bf
EduBatalha <eduardobatalha25@outlook.com>
2023-12-19 20:19:37 -0300

    Pequenos ajustes e implementação do carrossel de projetos

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
       <Projects />
       <div className='line'></div>
       <Skills />
+      <div className='line'></div>
       <Education />
       <div className='line'></div>
       <Experience />
